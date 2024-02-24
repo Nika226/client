b413caea3cbb2ce8efa1360e148557dc8d561cee
Veronika <veronika9ds114@gmail.com>
2024-02-24 23:47:05 +0100

    setup

---
+++ src/components/Contacts/index.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { SlSocialInstagram } from "react-icons/sl";
+import { SiWhatsapp } from "react-icons/si";
+import styles from "./index.module.css";
+
+export const Contacts = () => {
+  return (
+    <section className={styles.contacts}>
+      <div className={styles.links}>
+        <h2 className="title">Contacts</h2>
+        <Link className={styles.phone} to="tel:+49 999 999 99 99">
+          +49 999 999 99 99
+        </Link>
+        <ul className={styles.media_list}>
+          <li>
+            <Link
+              className={styles.media_item}
+              to="https://www.linkedin.com/school/tel-ran-de/"
+              target="_blank"
+            >
+              <SlSocialInstagram />
+              <p>instagram</p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.media_item}
+              to="https://wa.me/+499999999999"
+              target="_blank"
+            >
+              <SiWhatsapp />
+              <p>WhatsApp</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.address}>
+        <h2 className="title">Address</h2>
+        <address className={styles.address_wrapper}>
+          <Link>
+            <p className={styles.address_link}>Linkstraße 2, 8 OG, 10785,</p>
+            <span className={styles.address_link}>Berlin, Deutschland</span>
+          </Link>
+        </address>
+        <p className={styles.working_schedule}>Working Hours:</p>
+        <p className={styles.working_time}>24 hours a day</p>
+      </div>
+    </section>
+  );
+};
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import "./index.module.css";
-import Iframe from "react-iframe";
+import styles from "./index.module.css";
+import { Contacts, Map } from "../";
 
 const Footer = () => {
   return (
-    <div className="copyr">
-      <Iframe
-        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2428.4095566177666!2d13.372520476191944!3d52.507926837123975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zTGlua3N0cmHDn2XCoDIsIDjCoE9HLCAxMOKArzc4NSwgQmVybGluLCBEZXV0c2NobGFuZA!5e0!3m2!1sru!2sde!4v1705848208219!5m2!1sru!2sde"
-        width="100%"
-        height="400"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      />
-      <div>All rights reserved &copy 2023-2024</div>
-    </div>
+    <footer className={styles.footer}>
+      <div className="container">
+        <Contacts />
+        <Map />
+      </div>
+    </footer>
   );
 };
-
 export default Footer;
