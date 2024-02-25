3977c45208404b14842e3a487db1bcee3b206250
Veronika <veronika9ds114@gmail.com>
2024-02-25 16:42:57 +0100

    setup

---
+++ src/components/Offer/index.jsx
@@ -0,0 +1,33 @@
+import { PhoneForm } from "../";
+import imageGnome from "../../media/images/dwarf.png";
+import styles from "./Offer.module.css";
+
+export const Offer = () => {
+  return (
+    <section className={styles.offer}>
+      <div className="container">
+        <div className={styles.offer_wrapper}>
+          <div className={styles.image_wrapper}>
+            <img src={imageGnome} alt="garden gnome figurine" />
+          </div>
+
+          <div>
+            <h4 className={styles.offer_title}>
+              5% off
+              <span>on the first order</span>
+            </h4>
+            <PhoneForm
+              contentInput="sale"
+              placeholderInput="+49"
+              contentButton="sale_ofer"
+              textButton="Get a discount"
+              message="You have 5% discount for this phone number"
+              content="sale"
+              alertText="Get it"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/Mainpage/index.jsx
+++ src/pages/Mainpage/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Slayder from "../../components/Slayder/index.jsx";
 import SwiperComponent from "../../components/Swiper/index.jsx";
+import { Offer } from "../../components/Offer/index.jsx";
 
 const Mainpage = () => {
   return (
     <div>
       <Slayder />
       <SwiperComponent />
+      <Offer />
     </div>
   );
 };
